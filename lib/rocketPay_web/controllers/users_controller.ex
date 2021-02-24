5f637664471838d594a459d2bad53d0b38156852
Denis Melo <denis.melo@sambatech.com.br>
2021-02-24 18:04:19 -0300

    feat(user): added controller to create user

---
+++ lib/rocketPay_web/controllers/users_controller.ex
@@ -0,0 +1,15 @@
+defmodule RocketPayWeb.UsersController do
+  use RocketPayWeb, :controller
+
+  alias RocketPay.User
+
+  action_fallback RocketPayWeb.FallBackController
+
+  def create(conn, params) do
+    with {:ok, %User{} = user} <- RocketPay.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
+  end
+end
