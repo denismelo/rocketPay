a09fa73fa7815dd09861a8ede2131d64361621e1
Denis Melo <denis.melo@sambatech.com.br>
2021-02-24 18:12:25 -0300

    feat(account): added account facades

--- lib/rocketPay.ex
+++ lib/rocketPay.ex
@@ -1,5 +1,11 @@
 defmodule RocketPay do
   alias RocketPay.Users.Create, as: UserCreate
 
+  alias RocketPay.Accounts.Deposit
+  alias RocketPay.Accounts.WithDraw
+
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withDraw(params), to: WithDraw, as: :call
 end
