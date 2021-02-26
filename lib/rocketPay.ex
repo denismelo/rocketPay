3e3fb5924cae9f6d4b5645a73563983c5375647a
Denis Melo <denis.melo@sambatech.com.br>
2021-02-25 22:54:09 -0300

    feat(account_transaction): added transaction facade

--- lib/rocketPay.ex
+++ lib/rocketPay.ex
@@ -2,11 +2,13 @@ defmodule RocketPay do
   alias RocketPay.Users.Get, as: UserGet
   alias RocketPay.Users.Create, as: UserCreate
 
-  alias RocketPay.Accounts.{Deposit, WithDraw}
+  alias RocketPay.Accounts.{Deposit, WithDraw, Transaction}
 
   defdelegate get_user(params), to: UserGet, as: :call
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
   defdelegate withDraw(params), to: WithDraw, as: :call
+
+  defdelegate account_transaction(params), to: Transaction, as: :call
 end
